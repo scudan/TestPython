282a1b10605c6018b725e17cb18d2380a231ba6b
sundaY <sunxiaokun928@163.com>
2016-03-03 23:42:02 +0800

    zz
    zzz

---
+++ com/python/sundan/controlor.py
@@ -0,0 +1,9 @@
+a = 10;
+b = 10;
+
+if a > b :
+    print("a > b")
+elif (a == b):
+    print ("a = b")
+else:
+    print ("a < b")
---
+++ com/python/sundan/sysread.py
@@ -0,0 +1,4 @@
+import sys
+sys.stdout.write("Enter your name: ")
+name = sys.stdin.readline()
+print(name)
